ddb20526db36f0ba00cdf215d21d47e0a0cbb3a6
Uwe <uwe.helm@gmail.com>
2023-03-20 15:57:54 -0700

    lab 8

---
+++ lab 1/vagrant/node_app/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+
+http.listen(9000);
+console.log('App running at http://0.0.0.0' + ':' + '9000' + '/');
+
+app.get('/', (req, res) => {
+	console.log("HTTP request received from "+req.get("X-Forwarded-For")+" : "+req.method+" "+req.originalUrl);
+	console.log("\n" + "Received the following request headers: ");
+	console.log(req.headers);
+	var max = 999999999
+	var num = Math.sqrt(Math.random() * max) + 1;
+	var s = "Today's random number is: " + num;
+	res.status(200).send(s);
+});
